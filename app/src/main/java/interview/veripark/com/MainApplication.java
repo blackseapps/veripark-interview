2694eb9f29eb1544ca4e05a0b16d4abebac79b68
blackseapps <i@mertkaradeniz.com>
2021-11-06 02:01:32 +0300

    Dependencies added.

---
+++ app/src/main/java/interview/veripark/com/MainApplication.java
@@ -0,0 +1,17 @@
+package interview.veripark.com;
+
+import android.app.Application;
+
+/**
+ * Created by mertKaradeniz on 5.11.2021
+ * <p>
+ * This is an interview project.
+ */
+
+public class MainApplication extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
